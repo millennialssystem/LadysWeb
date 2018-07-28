d3dd7e7ad2b7b532f355d24a179fce9c83761d8a
millennialssystem <millennialssystem@gmail.com>
2018-07-28 08:24:29 -0400

    Creacion

---
+++ LadysWeb/LadysWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LadysWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
